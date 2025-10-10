84d081121c3b150fbfb17f77a8d14e879075e667
Лиза <anemiro827@gmail.com>
2025-10-10 12:30:59 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -3,5 +3,5 @@
 strings = ["hello", "world", "python"]
 # Генератор списка, который повторяет каждую строку 3 раза
 result = [string for string in strings for _ in range(3)]
-print(result)
+print(result) #Выводим результат действия на экран 
 # Вывод: ['hello', 'hello', 'hello', 'world', 'world', 'world', 'python', 'python', 'python']
